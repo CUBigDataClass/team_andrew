1edb110724a53f9b48368fbc3c191685c8bd361e
Drew Beathard <andrewpatrick97@gmail.com>
2022-03-17 15:29:21 -0600

    test image file works with reverse search, only for file directory right now

--- test_scraper.py
+++ test_scraper.py
@@ -7,27 +7,45 @@ import time
 Install selenium and chromedriver (make sure chromedriver matches your version of chrome)
 '''
 
-
-def download_image(url, folder_name, num):
-    # write image to file
-    response = requests.get(url)
-    if response.status_code==200:
-        with open(os.path.join(folder_name, str(num)+".jpg"),'wb') as file:
-            file.write(response.content)
-
 # download chromedriver and place it somewhere
 chromeDriverPath = "/usr/local/bin/chromedriver"
 driver = webdriver.Chrome(chromeDriverPath)
+
+
+try:
+    # Open the website
+    driver.get('https://images.google.com/')
+
+    # Find cam button
+    cam_button = driver.find_elements_by_xpath("//div[@aria-label=\"Search by image\" and @role=\"button\"]")[0]
+    cam_button.click()
+
+    # Find upload tab
+    upload_tab = driver.find_elements_by_xpath("//*[contains(text(), 'Upload an image')]")[0]
+    upload_tab.click()
+
+    # Find image input
+    upload_btn = driver.find_element_by_name('encoded_image')
+    upload_btn.send_keys(os.getcwd()+"/test_image.jpg")
+
+    # Click on "visually similar images"
+    driver.find_element_by_xpath("""//*[@id="rso"]/div[2]/div/div[2]/g-section-with-header/div[1]/title-with-lhs-icon/a/div[2]/h3""").click()
+
+    time.sleep(1)
+
+except Exception as e:
+    print(e)
 
 # create folder to save images
 image_path = "/Users/drewbeathard/Documents/big_data_files/image_scrape_testing"
 if not os.path.isdir(image_path):
     os.mkdir(image_path)
 
-search_URL = "https://www.google.com/search?q=cute+puppies&source=lnms&tbm=isch"
-driver.get(search_URL)
+# If we want to search by term
+# search_URL = "https://www.google.com/search?q=cute+puppies&source=lnms&tbm=isch"
+# driver.get(search_URL)
 
-num_images = 60
+num_images = 20
 
 
 # scroll down until we have enough images
@@ -54,8 +72,11 @@ for i in range(1,num_images + 1):
         continue
 
     xpath = """//*[@id="islrg"]/div[1]/div[%s]"""%(i)
-
+    image_website_xpath = """//*[@id="islrg"]/div[1]/div[%s]/a[2]"""%(i)
     preview_image_xpath = """//*[@id="islrg"]/div[1]/div[%s]/a[1]/div[1]/img"""%(i)
+
+
+    image_website_element = driver.find_element_by_xpath(image_website_xpath)
     preview_image_element = driver.find_element_by_xpath(preview_image_xpath)
     preview_image_url = preview_image_element.get_attribute("src")    
 
@@ -64,18 +85,18 @@ for i in range(1,num_images + 1):
     timeStarted = time.time()
     while True:
         image_element = driver.find_element_by_xpath("""//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div[1]/a/img""")
+        # gather image URL
         imageURL = image_element.get_attribute("src")
-        
+        image_description = image_element.get_attribute("alt")
+        image_website = image_website_element.get_attribute("href") 
         # if the new high-res image has loaded
         if imageURL != preview_image_url:
             break
 
         # if we have not loaded the high res images in x seconds, break
-        if time.time() - timeStarted > 7:
+        if time.time() - timeStarted > 3:
             break
 
-    try:
-        download_image(imageURL, image_path, i) 
-        print("Downloaded element %s out of %s total. URL: %s" % (i, num_images, imageURL))
-    except:
-            print("Couldn't download an image %s, continuing downloading the next one"%(i))
+    print("image description:", image_description)
+    print("image URL", imageURL)
+    print("image website:", image_website)
