03dd4b7cf61827913f21e5ff138d5938fd6447a0
Yubin Go(Sally) <54964332+yugo9081@users.noreply.github.com>
2022-03-21 22:48:10 -0600

    Clean the code

--- src/app.py
+++ src/app.py
@@ -12,25 +12,19 @@ def index():
 @app.route("/upload", methods=['POST'])
 def upload():
     target = os.path.join(APP_ROOT, 'images/')
-    # print(target)
 
     if not os.path.isdir(target):
         os.mkdir(target) #creat new folder if folder doesn't exist
     else:
         print("Folder already exists: {}".format(target))
-    # print(request.files.getlist("file"))
+
     for file in request.files.getlist("file"):
-        # print(file)
         filename = file.filename
         destination = "/".join([target, "temp.jpg"]) #change filename inside the target folder
-        # print(destination)
         file.save(destination)
-        #calling test_scraper.py for image scraping after the user clicks on Submite button.
+        #calling test_scraper.py for image scraping after the user clicks on Submit button.
         test_scraper.imageScrapping()
     return render_template("complete.html")
 
-# @app.route('/complete', methods=['POST', 'GET'])
-# def complete():
-#     return
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
