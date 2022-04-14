4e57a704adcc4a37496f34a6a4a47de2b8c232d7
karan171 <karanjuneja32@gmail.com>
2022-04-14 14:32:00 -0600

    made some changes in ner

--- Ner_module/select_top5.py
+++ Ner_module/select_top5.py
@@ -1,52 +1,10 @@
-from collections import Counter 
+from pymongo import MongoClient
+from ner_module import get_ner_data
 
-def select_top(lst):
-    #lst will be in format List[List[]]
-    flatList = [el for sublist in lst for el in sublist]
-    word_counts = Counter(flatList)
-    top_five = word_counts.most_common(5)
-    return top_five
-              
+my_client = MongoClient("mongodb+srv://team_andrew:Green@cluster1.jsqyd.mongodb.net/test")
+db = my_client.ImageSearch
+collection = db["Test"]
+x = collection.find()
 
-lst =  [
-      [
-        "cool",
-        "dog",
-        "breeds",
-        "cat",
-        "owners",
-        "5",
-        "easy",
-        "train",
-        "pups",
-        "hello"
-      ],
-      [
-        "10",
-        "cutest",
-        "dog",
-        "breeds"
-      ],
-      [
-        "10",
-        "cutest",
-        "dog",
-        "breeds",
-        "2022",
-        "love",
-        "goldens"
-      ],
-      [
-        "15",
-        "cute",
-        "dog",
-        "breeds",
-        "cool",
-        "cutest",
-        "able",
-        "resist",
-        "southern",
-        "living"
-      ]
-    ]
-print(select_top(lst))
+for i in x:
+    print(get_ner_data(i['description'],i['_id']))
