53f4bb7006242ef9ae2623b224dabe38909d3f71
karan171 <karanjuneja32@gmail.com>
2022-04-18 14:13:16 -0600

    added sanic support

--- src/app.py
+++ src/app.py
@@ -4,21 +4,26 @@ from flask import Flask, flash, redirect, render_template, send_from_directory, 
 import templates.test_scraper as test_scraper
 from templates.test_scraper import * #to import all variables from test_scraper.py
 import templates.deleteDB as deleteDB
+from sanic import Sanic
+from sanic.response import html
 
-app = Flask(__name__)
+app = Sanic(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+app.static('/static', './static')
+
 
 #Open Default homepage: upload.html
 @app.route("/")
-def index():
+async def index(request):
     #delete all data in database in homepage
     deleteDB.deleteAll()
-    return render_template("upload.html")
+    template = open(os.getcwd() + "/templates/upload.html")
+    return html(template.read())
 
 #Connect hompage with complete.html, and create upload and submit features
 @app.route("/upload", methods=['POST'])
-def upload():
+async def upload(request):
     target = os.path.join(APP_ROOT, 'images/')
 
     if not os.path.isdir(target):
@@ -34,11 +39,12 @@ def upload():
     #calling test_scraper.py for image scraping after the user clicks on Submit button.
     var = test_scraper.imgurl()
     var2 = test_scraper.imgweb()
+    template = open(os.getcwd() + "/templates/upload.html")
     return render_template("complete.html", image_name="temp.jpg", variable=var, variable2=var2)
 
 #Display uploaded image from user
 @app.route('/upload/<filename>')
-def send_image(filename):
+async def send_image(filename):
     return send_from_directory("images",filename)
 
 
