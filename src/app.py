ad97c97f5bf2cf9c94db2e2f11f910c006e9bd55
karan171 <karanjuneja32@gmail.com>
2022-04-23 14:03:50 -0600

    added host 0.0.0.0

--- src/app.py
+++ src/app.py
@@ -69,4 +69,4 @@ async def upload(request):
 #     return image_name
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True,workers=4)
+    app.run(host="0.0.0.0", port=5000, debug=True,workers=4)
